c46363f4fa43398f5031d95c7889b98956a0f786
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2024-02-28 18:42:31 +0500

    SQL

---
+++ Joining Functions 3.sql
@@ -0,0 +1,24 @@
+use Northwind
+--select *
+--from Region, Territories
+--where Territories.RegionID = Region.RegionID
+
+--select *
+--from Territories as Ter
+--INNER JOIN Region as Reg
+--on Ter.RegionID <> Reg.RegionID
+
+--select *
+--from Territories as Ter
+--FULL OUTER JOIN Region as Reg
+--on Ter.RegionID <> Reg.RegionID
+
+--select *
+--from Territories as Ter
+--LEFT JOIN Region as Reg
+--on Ter.RegionID <> Reg.RegionID
+
+--select *
+--from Territories as Ter
+--RIGHT JOIN Region as Reg
+--on Ter.RegionID <> Reg.RegionID
