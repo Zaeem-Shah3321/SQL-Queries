f6bf2a8afd0cb47d77b80aeae89104cd3ad2114d
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2024-02-28 18:44:11 +0500

    SQL

---
+++ Joining Functions 1.sql
@@ -0,0 +1,25 @@
+use DB1
+--create table Department
+--(
+--	DepID varchar (max),
+--	DepName varchar (max),
+--	ID varchar (max),
+--)
+
+--insert into Employee
+--values
+--(1 , 'Zaeem', 'UET'),
+--(2 , 'Chawan', 'Shaler'),
+--(3 , 'Moojn', 'Mugalpura'
+
+--insert into Department
+--values
+--('DE 1', 'CS', 1),
+--('DE 2' , 'CSE', 2)
+
+--select EmployeeName
+--from Employee,Department
+--where Employee.ID = Department.ID
+--AND Employee.Adress = Department.DepLocation
+
+
